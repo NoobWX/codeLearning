a03a2f009fbd95ecd7c2ec76ce0b2ee78900594e
nbWX <2387060795@qq.com>
2020-02-08 23:41:01 +0800

    Refactoring note

--- Demo/面对对象学习/动物运动会/Animal.cs
+++ Demo/面对对象学习/动物运动会/Animal.cs
@@ -1,3 +1,42 @@
+// class Animal {
+//     protected string name = "";
+//     public Animal () {
+//         this.name = "无名";
+//     }
+//     Public Animal (string name) {
+//         this.name = name;
+//     }
+//     protected int shutNum = 3;
+//     public int ShutNum {
+//         get { return shutNum; }
+//         set { shutNum = value; }
+//     }
+//     public virtual string Shut () {
+//         return "";
+//     }
+// }
+// class Cat : Animal {
+//     public Cat () : base () { }
+//     public Cat (string name) : base (name) { }
+//     public override string Shut () {
+//         string Lines = "";
+//         for (int i = 0; i < ShutNum; ++i) {
+//             Lines += "喵";
+//         }
+//         return "我的名字叫" + name + "  " + Lines;
+//     }
+// }
+// class Dog : Animal {
+//     public Dog () : base () { }
+//     public Dog (string name) : base (name) { }
+//     public override string Shut () {
+//         string Lines = "";
+//         for (int i = 0; i < ShutNum; ++i) {
+//             Lines += "汪";
+//         }
+//         return "我的名字叫" + name + "   " + Lines;
+//     }
+// }
 class Animal {
     protected string name = "";
     public Animal () {
@@ -11,29 +50,41 @@ class Animal {
         get { return shutNum; }
         set { shutNum = value; }
     }
-    public virtual string Shut () {
+    public string Shut () {
+        string lines = "我叫" + name;
+        for (int i = 0; i < ShutNum; ++i) {
+            lines += getSound ();
+        }
+    }
+    protected virtual string getSound () {
         return "";
     }
 }
 class Cat : Animal {
     public Cat () : base () { }
     public Cat (string name) : base (name) { }
-    public override string Shut () {
-        string Lines = "";
-        for (int i = 0; i < ShutNum; ++i) {
-            Lines += "喵";
-        }
-        return "我的名字叫" + name + "  " + Lines;
+    public override string getSound () {
+        return "喵";
     }
 }
 class Dog : Animal {
     public Dog () : base () { }
     public Dog (string name) : base (name) { }
-    public override string Shut () {
-        string Lines = "";
-        for (int i = 0; i < ShutNum; ++i) {
-            Lines += "汪";
-        }
-        return "我的名字叫" + name + "   " + Lines;
+    public override string getSound () {
+        return "汪";
+    }
+}
+class Cattle : Animal {
+    public Cattle () : base () { }
+    public Cattle (string name) : base (name) { }
+    public override string getSound () {
+        return "哞";
+    }
+}
+class Sheep : Animal {
+    public Sheep () : base () { }
+    public Sheep (string name) : base (name) { }
+    public override string getSound () {
+        return "咩";
     }
 }
--- Demo/面对对象学习/动物运动会/Main.cs
+++ Demo/面对对象学习/动物运动会/Main.cs
@@ -1,11 +1,16 @@
 class Program {
     public static void Main (string[] args) {
         Animal[] arrayAnimal = new Animal[5];
-        arrayAnimal[0] = new Cat ("小白");
-        arrayAnimal[1] = new Dog ("小黑");
-        arrayAnimal[2] = new Cat ("小莉");
-        arrayAnimal[3] = new Cat ("咪咪");
-        arrayAnimal[4] = new Dog ("旺财");
+        // arrayAnimal[0] = new Cat ("小白");
+        // arrayAnimal[1] = new Dog ("小黑");
+        // arrayAnimal[2] = new Cat ("小莉");
+        // arrayAnimal[3] = new Cat ("咪咪");
+        // arrayAnimal[4] = new Dog ("旺财");
+        arrayAnimal[0] = new Cattle("小花");
+        arrayAnimal[1] = new Cat("咪咪");
+        arrayAnimal[2] = new Dog("小黑");
+        arrayAnimal[3] = new Sheep("小咩");
+        arrayAnimal[4] = new Cat("喵喵");
         foreach (Animal item in arrayAnimal) {
             System.Console.WriteLine (item.Shut ());
         }
