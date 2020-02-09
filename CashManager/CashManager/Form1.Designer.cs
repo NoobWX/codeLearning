9755e5a540b0f3ae4b1cce0d396a90938d3f8952
nbWX <2387060795@qq.com>
2020-02-09 17:35:57 +0800

    《大话设计模式》商场收银系统——策略模式 CashManager v1.1

--- CashManager/CashManager/Form1.Designer.cs
+++ CashManager/CashManager/Form1.Designer.cs
@@ -37,12 +37,14 @@
             this.item_lbx = new System.Windows.Forms.ListBox();
             this.label3 = new System.Windows.Forms.Label();
             this.result_lbl = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Promotion_cbx = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(96, 67);
+            this.label1.Location = new System.Drawing.Point(107, 36);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(62, 18);
             this.label1.TabIndex = 0;
@@ -51,7 +53,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(96, 123);
+            this.label2.Location = new System.Drawing.Point(107, 92);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(62, 18);
             this.label2.TabIndex = 1;
@@ -59,21 +61,21 @@
             // 
             // Price_tbx
             // 
-            this.Price_tbx.Location = new System.Drawing.Point(189, 61);
+            this.Price_tbx.Location = new System.Drawing.Point(200, 30);
             this.Price_tbx.Name = "Price_tbx";
             this.Price_tbx.Size = new System.Drawing.Size(169, 28);
             this.Price_tbx.TabIndex = 2;
             // 
             // Counts_tbx
             // 
-            this.Counts_tbx.Location = new System.Drawing.Point(189, 117);
+            this.Counts_tbx.Location = new System.Drawing.Point(200, 86);
             this.Counts_tbx.Name = "Counts_tbx";
             this.Counts_tbx.Size = new System.Drawing.Size(169, 28);
             this.Counts_tbx.TabIndex = 3;
             // 
             // OK_btn
             // 
-            this.OK_btn.Location = new System.Drawing.Point(415, 54);
+            this.OK_btn.Location = new System.Drawing.Point(426, 23);
             this.OK_btn.Name = "OK_btn";
             this.OK_btn.Size = new System.Drawing.Size(75, 39);
             this.OK_btn.TabIndex = 4;
@@ -83,7 +85,7 @@
             // 
             // Reset_btn
             // 
-            this.Reset_btn.Location = new System.Drawing.Point(415, 112);
+            this.Reset_btn.Location = new System.Drawing.Point(426, 81);
             this.Reset_btn.Name = "Reset_btn";
             this.Reset_btn.Size = new System.Drawing.Size(75, 35);
             this.Reset_btn.TabIndex = 5;
@@ -119,11 +121,31 @@
             this.result_lbl.TabIndex = 8;
             this.result_lbl.Text = "0.00";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(107, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "优惠策略：";
+            // 
+            // Promotion_cbx
+            // 
+            this.Promotion_cbx.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Promotion_cbx.FormattingEnabled = true;
+            this.Promotion_cbx.Location = new System.Drawing.Point(200, 141);
+            this.Promotion_cbx.Name = "Promotion_cbx";
+            this.Promotion_cbx.Size = new System.Drawing.Size(169, 26);
+            this.Promotion_cbx.TabIndex = 10;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(596, 517);
+            this.Controls.Add(this.Promotion_cbx);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.result_lbl);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.item_lbx);
@@ -135,6 +157,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "商场收银系统";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,6 +174,8 @@
         private System.Windows.Forms.ListBox item_lbx;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label result_lbl;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox Promotion_cbx;
     }
 }
 
--- CashManager/CashManager/Form1.cs
+++ CashManager/CashManager/Form1.cs
@@ -17,21 +17,37 @@ namespace CashManager
         {
             InitializeComponent();
         }
-
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Promotion_cbx.Items.AddRange (new object[] { "正常收费", "八折优惠" });
+            Promotion_cbx.SelectedIndex = 0;
+        }
         private void OK_btn_Click(object sender, EventArgs e)
         {
-            total += Convert.ToDouble(Price_tbx.Text)*Convert.ToDouble(Counts_tbx.Text);
-            item_lbx.Items.Add("单价："+Price_tbx.Text+"元;  数量："+Counts_tbx.Text+"件");
+            switch(Promotion_cbx.SelectedIndex)
+            {
+                case 0: 
+                    total += Convert.ToDouble(Price_tbx.Text) * Convert.ToDouble(Counts_tbx.Text);
+
+                    break;
+                case 1:
+                    total += Convert.ToDouble(Price_tbx.Text) * Convert.ToDouble(Counts_tbx.Text) * 0.8;
+                    break;
+            }
+            item_lbx.Items.Add("单价："+Price_tbx.Text+"元;  数量："+Counts_tbx.Text+"件;   "+Promotion_cbx.Text);
             result_lbl.Text = total.ToString()+"元";
         }
 
         private void Reset_btn_Click(object sender, EventArgs e)
         {
             Price_tbx.Text = "";
+            Price_tbx.Text = "";
             Counts_tbx.Text = "";
-            result_lbl.Text = ""+"元";
+            result_lbl.Text = "";
             item_lbx.Items.Clear();
 
         }
+
+
     }
 }
