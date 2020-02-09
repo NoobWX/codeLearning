b092d6e507abb121ec723e7b936eea71f9f970f2
nbWX <2387060795@qq.com>
2020-02-10 00:45:52 +0800

    简单工厂模式（向策略模式过渡） CashManager v1.2

---
+++ CashManager/CashManager/CashFun.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CashManager
+{
+    abstract class CashFun
+    { 
+        protected int Count = 0;
+        protected double Price = 0;
+        protected double Debate = 0.0d;
+        public CashFun(double Debate,int Count,double Price)
+        {
+            this.Debate = Debate;
+            this.Count = Count;
+            this.Price = Price;
+        }
+        public abstract double GetResult();
+    }
+    class CashCost : CashFun
+    {
+        public CashCost(double Debate,int Count,double Price) : base(Debate,Count,Price)
+        { }
+        public override double GetResult()
+        {
+            return Count * Price;
+        }
+    }
+    class CashRecost : CashFun
+    {
+        public CashRecost(double Debate, int Count, double Price) : base(Debate, Count, Price)
+        { }
+        public override double GetResult()
+        {
+            return Count * Price - Debate;
+        }
+    }
+    class CashDiscount : CashFun
+    {
+        public CashDiscount(double Debate, int Count, double Price) : base(Debate, Count, Price)
+        { }
+        public override double GetResult()
+        {
+            return Count * Price * Debate;
+        }
+    }
+    class CreateCashFactory
+    {
+        private int _count = 0;
+        private double _price = 0.0d;
+        public CreateCashFactory(string count,string price)
+        {
+            this._count = Convert.ToInt32(count);
+            this._price = Convert.ToDouble(price);
+        }
+        public CashFun CreateCashFun(int selectedindex)
+        {
+            CashFun cf = null;
+            try
+            {
+                switch (selectedindex)
+                {
+                    case 0:
+                        cf = new CashCost(1,_count,_price);
+                        return cf;
+                    case 1:
+                        cf = new CashDiscount(0.8,_count,_price);
+                        return cf;
+                    case 2:
+                        if (_count * _price >= 300)
+                        {
+                            cf = new CashRecost(100,_count,_price);
+                            return cf;
+                        }
+                        else
+                        {
+                            cf = new CashCost(1,_count,_price);
+                            return cf;
+                        }
+                    default:
+                        return cf;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("尚未添加此优惠方案");
+                return null;
+            }
+        }
+    }
+}
--- CashManager/CashManager/Form1.cs
+++ CashManager/CashManager/Form1.cs
@@ -19,23 +19,29 @@ namespace CashManager
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-            Promotion_cbx.Items.AddRange (new object[] { "正常收费", "八折优惠" });
+            Promotion_cbx.Items.AddRange (new object[] { "正常收费", "八折优惠","满300减100" });
             Promotion_cbx.SelectedIndex = 0;
         }
         private void OK_btn_Click(object sender, EventArgs e)
         {
-            switch(Promotion_cbx.SelectedIndex)
-            {
-                case 0: 
-                    total += Convert.ToDouble(Price_tbx.Text) * Convert.ToDouble(Counts_tbx.Text);
-
-                    break;
-                case 1:
-                    total += Convert.ToDouble(Price_tbx.Text) * Convert.ToDouble(Counts_tbx.Text) * 0.8;
-                    break;
-            }
-            item_lbx.Items.Add("单价："+Price_tbx.Text+"元;  数量："+Counts_tbx.Text+"件;   "+Promotion_cbx.Text);
+            CreateCashFactory ccf = new CreateCashFactory(Counts_tbx.Text, Price_tbx.Text);
+            CashFun cf = ccf.CreateCashFun(Promotion_cbx.SelectedIndex);
+            item_lbx.Items.Add("单价：" + Price_tbx.Text + "元;  数量：" + Counts_tbx.Text + "件;   " + Promotion_cbx.Text);
+            total += cf.GetResult();
             result_lbl.Text = total.ToString()+"元";
+
+            //switch(Promotion_cbx.SelectedIndex)
+            //{
+            //    case 0: 
+            //        total += Convert.ToDouble(Price_tbx.Text) * Convert.ToDouble(Counts_tbx.Text);
+
+            //        break;
+            //    case 1:
+            //        total += Convert.ToDouble(Price_tbx.Text) * Convert.ToDouble(Counts_tbx.Text) * 0.8;
+            //        break;
+            //}
+            //item_lbx.Items.Add("单价："+Price_tbx.Text+"元;  数量："+Counts_tbx.Text+"件;   "+Promotion_cbx.Text);
+            //result_lbl.Text = total.ToString()+"元";
         }
 
         private void Reset_btn_Click(object sender, EventArgs e)
@@ -43,9 +49,8 @@ namespace CashManager
             Price_tbx.Text = "";
             Price_tbx.Text = "";
             Counts_tbx.Text = "";
-            result_lbl.Text = "";
-            item_lbx.Items.Clear();
-
+            result_lbl.Text = "0.0";
+            item_lbx.Items.Clear(); 
         }
 
 
