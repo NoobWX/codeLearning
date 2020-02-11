9db41d8bd61c822f1bb9ec7760611425c503318f
nbWX <2387060795@qq.com>
2020-02-12 02:31:32 +0800

    初识策略模式

--- StrategyCode/StrategyExpel/StrategyExpel/Context.cs
+++ StrategyCode/StrategyExpel/StrategyExpel/Context.cs
@@ -6,7 +6,7 @@ namespace StrategyExpel
 {
     class Context
     {
-        Strategy strategy;
+        private readonly Strategy strategy;//我也不知道为啥提示我要加个readonly，private和readonly可以不加的，仅仅是代码划线强迫症看着不爽。
         public Context(Strategy strategy)
         {
             this.strategy = strategy;
